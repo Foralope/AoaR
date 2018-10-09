4d56c92fac8aae193ccab542acf6d105989c20fc
wizwa9381 <wizwa9381@7KZM6Y1.Student.UGDSB.ED>
2018-10-09 09:10:16 -0400

    AoaR

---
+++ src/area/of/a/rectangle/AreaOfARectangle.java
@@ -0,0 +1,28 @@
+/*
+ * William Zwart
+ * 18/9/18
+ * This should calculate the area of a rectangle, and display it along with width and length
+ */
+package area.of.a.rectangle;
+
+/**
+ *
+ * @author wizwa9381
+ */
+public class AreaOfARectangle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double length = 5.7;
+        double width = 4.8;
+        double area = 0.0;
+        area = length * width;
+        System.out.println("The area is " + area);
+        System.out.println("The length is " + length);
+        System.out.println("The width is " + width);
+        // TODO code application logic here
+    }
+    
+}
